1a3b37878db3d35792f9a977430ed343f157df60
Tender Wang <tndrwang@gmail.com>
2026-03-18 16:59:33 +0800

    Do some refactor

--- src/backend/postmaster/arbiter.c
+++ src/backend/postmaster/arbiter.c
@@ -57,8 +57,11 @@ static ArbiterShmemState *ArbiterState = NULL;
 static void ProcessArbiterInterrupts();
 static bool TryPromoteSlave(bool wait, int wait_seconds);
 static AlterSystemStmt* CreateDummyASCommand(uint64 gen, int leader_id);
-
-extern bool Atomic_CAS_Disk(int fd, uint64 old_gen, ArbiterControlData *new_ctrl);
+static void ReadClusterControlFile(void);
+static void WriteClusterControlFile(void);
+static void UpdateClusterControlFile();
+static void PromoteToLeader(cluster_type *role, bool *need_reconnect, uint64 *gen,
+							int *primary_id);
 
 /* GUC check_hook for node_id */
 bool
@@ -232,6 +235,172 @@ TryPromoteSlave(bool wait, int wait_seconds)
 	return false;
 }
 
+static void
+ReadClusterControlFile(void)
+{
+	int fd;
+	int r;
+
+	fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
+					   O_RDWR | PG_BINARY);
+	if (fd < 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not open file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	r = read(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData));
+	if (r != sizeof(ArbiterControlData))
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not read file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+
+	if (close(fd) != 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not close file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+}
+
+static void
+WriteClusterControlFile(void)
+{
+	int fd;
+
+	fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
+					   O_RDWR | PG_BINARY | O_CREAT);
+	if (fd < 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not open file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	/* We must overwrite old data */
+	if (lseek(fd, 0, SEEK_SET) == (off_t)-1)
+	{
+		ereport(ERROR,
+				(errcode_for_file_access(),
+				 errmsg("could not seek in file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	}
+	if (write(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData)) !=
+		sizeof(ArbiterControlData))
+	{
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not write to file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	}
+	(void)pg_fsync(fd);
+	if (close(fd) != 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not close file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+}
+
+static void
+UpdateClusterControlFile()
+{
+	int fd;
+
+	fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
+					   O_RDWR | PG_BINARY | O_CREAT);
+	if (fd < 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not open file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	SpinLockAcquire(&ArbiterState->mutext);
+	ArbiterState->ctl_data.heartbeat_ts = (pg_time_t) time(NULL);
+	SpinLockRelease(&ArbiterState->mutext);
+	/* We must overwrite old data */
+	if (lseek(fd, 0, SEEK_SET) == (off_t)-1)
+	{
+		ereport(ERROR,
+				(errcode_for_file_access(),
+				 errmsg("could not seek in file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	}
+	if (write(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData)) !=
+		sizeof(ArbiterControlData))
+	{
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not write to file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	}
+	(void)pg_fsync(fd);
+	if (close(fd) != 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not close file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+}
+
+/*
+ * Slaves try to acquire the leadership
+ * Only one can success, others should re-connect to the
+ * new leader
+ */
+static void
+PromoteToLeader(cluster_type *role, bool *need_reconnect, uint64 *gen, int *primary_id)
+{
+	struct flock fl;
+	int fd;
+	int r;
+
+	fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
+					   O_RDWR | PG_BINARY);
+	if (fd < 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not open file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+
+	fl.l_type = F_WRLCK;
+	fl.l_whence = SEEK_SET;
+	fl.l_start = 0;
+	fl.l_len = sizeof(ArbiterControlData);
+	if (fcntl(fd, F_SETLK, &fl) == -1)
+	{
+		close(fd);
+		return;
+	}
+	r = read(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData));
+	if (r != sizeof(ArbiterControlData))
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not read file \"%s\": %m",
+						CLUSTER_CONTROL_FILE)));
+	SpinLockAcquire(&ArbiterState->mutext);
+	if (*gen == ArbiterState->ctl_data.generation &&
+		*primary_id == ArbiterState->ctl_data.leader_node_id)
+	{
+		ArbiterState->ctl_data.generation++;
+		ArbiterState->ctl_data.leader_node_id = node_id;
+		ArbiterState->ctl_data.heartbeat_ts = (pg_time_t)time(NULL);
+		snprintf(ArbiterState->ctl_data.leader_conn, 256, "user=%s host=%s port=%d",
+				 "ubuntu", "localhost", PostPortNumber);
+		*role = primary;
+	}
+	else
+	{
+		/* leader has changed */
+		/* walreceiver needs to connect the new leader */
+		*need_reconnect = true;
+	}
+	*gen = ArbiterState->ctl_data.generation;
+	*primary_id = ArbiterState->ctl_data.leader_node_id;
+	SpinLockRelease(&ArbiterState->mutext);
+	fl.l_type = F_UNLCK;
+	fcntl(fd, F_SETLK, &fl);
+	if (close(fd) != 0)
+		ereport(PANIC,
+				(errcode_for_file_access(),
+				 errmsg("could not close file \"%s\": %m",
+				 CLUSTER_CONTROL_FILE)));
+}
+
 /*
  * Entry point for arbiter process.
  */
@@ -239,12 +408,9 @@ void
 ArbiterMain(const void *startup_data, size_t startup_data_len)
 {
 	bool continue_sleep;
-	int fd;
-	int r;
 	bool do_promote = false;
 	struct stat stat_buf;
 	int local_primary_id = 0;
-	pg_time_t now;
 	uint64 local_generation = 0;
 	cluster_type local_cluster_role = cluster_role;
 	bool need_reconnect = false;
@@ -276,41 +442,12 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 
 	if (local_cluster_role == primary)
 	{
-		now = (pg_time_t) time(NULL);
 		if (stat(CLUSTER_CONTROL_FILE, &stat_buf) == 0)
 		{
-			fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
-					   O_RDWR | PG_BINARY);
-			if (fd < 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-				 		 errmsg("could not open file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			r = read(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData));
-			if (r != sizeof(ArbiterControlData))
-				ereport(PANIC,
-						(errcode_for_file_access(),
-					 	 errmsg("could not read file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			/* check the data in the cluster control file */
+			/* read cluster control file */
+			ReadClusterControlFile();
 			if (node_id != ArbiterState->ctl_data.leader_node_id)
 				elog(PANIC, "node_id should equal with value in the cluster control file");
-			SpinLockAcquire(&ArbiterState->mutext);
-			ArbiterState->ctl_data.heartbeat_ts = now;
-			SpinLockRelease(&ArbiterState->mutext);
-			if (write(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData)) !=
-					  sizeof(ArbiterControlData))
-			{
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not write to file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			}
-			if (close(fd) != 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not close file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
 		}
 		else
 		{
@@ -320,35 +457,15 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 						(errcode_for_file_access(),
 					 	 errmsg("could not stat file \"%s\": %m",
 								CLUSTER_CONTROL_FILE)));
-			/* Cluster is first startup, we should create the control file */
-			fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
-					   O_RDWR | PG_BINARY|O_CREAT);
-			if (fd < 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-				 		 errmsg("could not open file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
 			SpinLockAcquire(&ArbiterState->mutext);
-			ArbiterState->ctl_data.heartbeat_ts = now;
+			ArbiterState->ctl_data.heartbeat_ts = (pg_time_t) time(NULL);
 			ArbiterState->ctl_data.generation++;
 			ArbiterState->ctl_data.leader_node_id = node_id;
-			snprintf(ArbiterState->ctl_data.leader_conn, 256,"user=%s host=%s port=%d",
+			snprintf(ArbiterState->ctl_data.leader_conn, 256, "user=%s host=%s port=%d",
 					 "ubuntu", "localhost", PostPortNumber);
 			SpinLockRelease(&ArbiterState->mutext);
-			if (write(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData)) !=
-					  sizeof(ArbiterControlData))
-			{
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not write to file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			}
-			(void) pg_fsync(fd);
-			if (close(fd) != 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not close file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
+			/* Cluster is startup, we should create the control file */
+			WriteClusterControlFile();
 		}
 	}
 
@@ -365,23 +482,12 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 		if (local_cluster_role == primary)
 		{
 			/* update hearbeat */
+			UpdateClusterControlFile(false);
 		}
 		else
 		{
 			/* read cluster control file */
-			fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
-					   O_RDWR | PG_BINARY);
-			if (fd < 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-				 		 errmsg("could not open file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			r = read(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData));
-			if (r != sizeof(ArbiterControlData))
-				ereport(PANIC,
-						(errcode_for_file_access(),
-					 	 errmsg("could not read file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
+			ReadClusterControlFile();
 			/* check the data in the cluster control file */
 			if (node_id == ArbiterState->ctl_data.leader_node_id)
 				elog(PANIC, "node_id should not equal with value in the cluster control file");
@@ -389,11 +495,6 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 			local_primary_id = ArbiterState->ctl_data.leader_node_id;
 			local_generation = ArbiterState->ctl_data.generation;
 			SpinLockRelease(&ArbiterState->mutext);
-			if (close(fd) != 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not close file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
 		}
 
 		/* check if walreceiver process is live */
@@ -401,79 +502,16 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 			!WalRcvRunning() &&
 			RecoveryInProgress())
 		{
-			/* Primary has gone away, try to be the new primary */
-			struct flock fl;
-			fd = BasicOpenFile(CLUSTER_CONTROL_FILE,
-					   O_RDWR | PG_BINARY);
-			if (fd < 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-				 		 errmsg("could not open file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
+			/* The leader has gone away, try to be the new leader */
+			PromoteToLeader(&local_cluster_role, &need_reconnect,
+							&local_generation, &local_primary_id);
 
-			fl.l_type = F_WRLCK;
-			fl.l_whence = SEEK_SET;
-			fl.l_start = 0;
-			fl.l_len = sizeof(ArbiterControlData);
-			if (fcntl(fd, F_SETLK, &fl) == -1)
-			{
-				close(fd);
-				goto WAIT_MOMENT;
-			}
-			r = read(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData));
-			if (r != sizeof(ArbiterControlData))
-				ereport(PANIC,
-						(errcode_for_file_access(),
-					 	 errmsg("could not read file \"%s\": %m",
-							CLUSTER_CONTROL_FILE)));
-			SpinLockAcquire(&ArbiterState->mutext);
-			if (local_generation == ArbiterState->ctl_data.generation &&
-				local_primary_id == ArbiterState->ctl_data.leader_node_id)
-			{
-				ArbiterState->ctl_data.generation++;
-				ArbiterState->ctl_data.leader_node_id = node_id;
-				ArbiterState->ctl_data.heartbeat_ts = (pg_time_t) time(NULL);
-				snprintf(ArbiterState->ctl_data.leader_conn, 256,"user=%s host=%s port=%d",
-					 "ubuntu", "localhost", PostPortNumber);
-				local_cluster_role = primary;
-			}
-			else
-			{
-				/* leader has changed */
-				/* walreceiver needs to connect the new leader */
-				need_reconnect = true;
-			}
-			local_generation = ArbiterState->ctl_data.generation;
-			local_primary_id = ArbiterState->ctl_data.leader_node_id;
-			SpinLockRelease(&ArbiterState->mutext);
 			if (local_cluster_role == primary)
 			{
-				/* We must overwrite old data */
-				if (lseek(fd, 0, SEEK_SET) == (off_t) -1)
-				{
-					ereport(ERROR,
-							(errcode_for_file_access(),
-							 errmsg("could not seek in file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
-				}
 				/* We are the new leader */
-				if (write(fd, &ArbiterState->ctl_data, sizeof(ArbiterControlData)) !=
-					  sizeof(ArbiterControlData))
-				{
-					ereport(PANIC,
-							(errcode_for_file_access(),
-							 errmsg("could not write to file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
-				}
-				(void) pg_fsync(fd);
+				WriteClusterControlFile();
 			}
-			fl.l_type = F_UNLCK;
-			fcntl(fd, F_SETLK, &fl);
-			if (close(fd) != 0)
-				ereport(PANIC,
-						(errcode_for_file_access(),
-						 errmsg("could not close file \"%s\": %m",
-								CLUSTER_CONTROL_FILE)));
+			
 			/*
 			 * Now we simply assume the primary has gone away.
 			 * Then we promote the slave to be a new primary.
@@ -482,15 +520,18 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 			 */
 			if (!do_promote && (local_cluster_role == primary))
 			{
-				/*We are selected to the new leader */
-				/* We shoulde promote ourself */
+				/*
+				 * We are selected to the new leader
+				 * We shoulde promote ourself
+				 */
 				elog(LOG, "Try to promote a slave");
 				do_promote = TryPromoteSlave(false, 1);
 			}
 			else if (need_reconnect)
 			{
-				/* We are not selected to be the leader
-				 * We must reconnect the new leader
+				/* 
+				 * We are not selected to be the leader,
+				 * then we should reconnect the new leader
 				 */
 				Oid			save_userid = 0;
 				int			save_sec_context = 0;
@@ -500,7 +541,7 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 									   save_sec_context | SECURITY_LOCAL_USERID_CHANGE);
 				IsUnderPostmaster = false;
 				ass = CreateDummyASCommand(local_generation, local_primary_id);
-				/*Note: We must be superuser */
+				/* XXX: We must be superuser */
 				if (ass != NULL)
 					AlterSystemSetConfigFile(ass);
 				SetUserIdAndSecContext(save_userid, save_sec_context);
@@ -509,7 +550,6 @@ ArbiterMain(const void *startup_data, size_t startup_data_len)
 			}
 		}
 
-WAIT_MOMENT:
 		/* sleep... */
 		rc = WaitLatch(MyLatch,
 					   WL_LATCH_SET | WL_TIMEOUT | WL_POSTMASTER_DEATH,
